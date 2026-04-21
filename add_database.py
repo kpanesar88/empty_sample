740095b51d8215bce5850abb57850202b1807be9
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2026-04-20 22:16:49 -0400

    defined functions in database

--- add_database.py
+++ add_database.py
@@ -1,43 +1,46 @@
 import json
+from read_email import get_parsed_unread_emails
 
 filename = "dataset/dataset.json"
-next_id = ""
 
 
-rv_company = ""
-rv_job_title=""
-rv_sender=""
-rv_subject=""
-rv_body=""
-rv_status=""
-rv_id = next_id
+def load_database(filename):
+    with open(filename, "r", encoding="utf-8") as json_file:
+        return json.load(json_file)
 
 
-
-def add_email(data, filename):
+def save_database(data, filename):
     with open(filename, "w", encoding="utf-8") as database:
-        json.dump(data, database, indent = 4)
-        
-        if len(data["emails"] == 0):
-            next_id = 1
-        else: 
-            next_id = data["emails"][-1]["id"] + 1
+        json.dump(data, database, indent=4)
 
 
-with open(filename, "r", encoding="utf-8") as json_file:
-    data = json.load(json_file)
-    
-    new_email = {
-    "id": 61,
-    "company": "TEST",
-    "job_title": "Engineering Intern",
-    "sender": "recruitment@peakroute.com",
-    "subject": "TEST",
-    "body": "We are pleased to let you know that you have been moved forward to the next stage.",
-    "status": "next_step"
-     }
-    
-    data["emails"].append(new_email)
-    
-add_email(data,filename)
-    
+def get_next_id(data):
+    if not data["emails"]:
+        return 1
+    return data["emails"][-1]["id"] + 1
+
+
+def add_emails_to_database():
+    data = load_database(filename)
+    unread_emails = get_parsed_unread_emails()
+
+    next_id = get_next_id(data)
+
+    for email_info in unread_emails:
+        new_email = {
+            "id": next_id,
+            "company": "",
+            "job_title": "",
+            "sender": email_info["sender"],
+            "subject": email_info["subject"],
+            "body": email_info["body"],
+            "status": ""
+        }
+
+        data["emails"].append(new_email)
+        next_id += 1
+
+    save_database(data, filename)
+
+
+add_emails_to_database()
